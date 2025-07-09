85f9726754bfc3aadc0f644a9124f0631781c8b9
Jinwen Zou <jzou25@stanford.edu>
2025-07-09 14:51:30 -0700

    manual granted permission

---
+++ overlays/prod/embargo-prod/sql/grant.sql
@@ -0,0 +1,4 @@
+\c lsstdb1
+grant all on all sequences in schema embargo to campaign_nv;
+grant all on all sequences in schema embargo to campaign_drp;
+grant all on all sequences in schema embargo to rapid_analysis;
